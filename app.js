3edb3e07a12bbf5c0b4bd737e499108c81846542
Ryan Wire <ryanwire@outlook.com>
2019-06-16 03:47:31 +0300

    Sets up dotenv

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 
 // Set up the express app
 const app = express();
 
 // Log request to the console
 app.use(logger('dev'));
+
+dotenv.config();
 
 // Parse incoming requests data
 app.use(bodyParser.json());
